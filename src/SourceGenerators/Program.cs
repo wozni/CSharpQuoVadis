f53ed7bc4e2fde62e9629ea1d230521e28cc81ac
mwozniak <mwozniak@teldat.local>
2021-12-05 13:20:21 +0100

    WIP

---
+++ src/SourceGenerators/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AutoSettings;
+
+var a = XmlSettings.Main;
+Console.WriteLine($"FirstRun = {XmlSettings.Main.FirstRun}");
+Console.WriteLine($"CacheSize = {XmlSettings.Main.CacheSize}");
+Console.WriteLine($"ValidTo = {XmlSettings.Main.ValidTo.AddYears(10)}");
+
+
